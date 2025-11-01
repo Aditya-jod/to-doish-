871ad7c99856ff78a4a41d65cdcba2c7ed22cecc
Aditya-jod <aditya1710j@gmail.com>
2025-11-01 12:26:01 +0530

    Port change for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const taskRoutes = require('./routes/tasks');
 const routineRoutes = require('./routes/routines');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Connect to database
 connectDB();
@@ -49,5 +49,5 @@ app.get('/register', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
